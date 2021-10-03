9c5351e2030522fab9ca193e02c2462afab28ca5
Daniel Haden <haden.daniel@gmail.com>
2021-10-03 15:22:26 -0600

    Added minimum to template

---
+++ WPFDesktopGUITemplate/ViewModels/ShellViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Caliburn.Micro;
+using System.Threading;
+
+namespace WPFDesktopGUITemplate.ViewModels
+{
+    class ShellViewModel : Conductor<object>
+    {
+
+        public ShellViewModel()
+        {
+            ActivateItemAsync(IoC.Get<DemoViewModel>(), new CancellationToken());
+        }
+
+    }
+}
