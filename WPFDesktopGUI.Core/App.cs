db889083d74d737127643bde0dc52736fb244533
Daniel Haden <haden.daniel@gmail.com>
2021-10-03 19:05:23 -0600

    Added minimum classes to core

---
+++ WPFDesktopGUI.Core/App.cs
@@ -0,0 +1,27 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDesktopGUI.Core
+{
+    public class App : MvxApplication
+    {
+
+        /// <summary>
+        /// Runs in background on application startup
+        /// </summary>
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<HomeViewModel>();
+        }
+    }
+}
